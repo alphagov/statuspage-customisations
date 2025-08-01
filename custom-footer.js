bc126e2c1e2dd8405f101d683d86d036ea489ff4
Tom Byers <tombaromba@gmail.com>
2025-08-01 22:54:41 +0100

    Make changes to JS based on Standard JS rules

--- custom-footer.js
+++ custom-footer.js
@@ -1,132 +1,132 @@
-$(function() {
+$(function () {
   // add skiplink and link to main content
-  var pathRoot = "/" + window.location.pathname.split('/')[1];
-  var mainContainerMap = {
-    "/" : ".masthead-container + .container",
-    "/history": ".months-container",
-    "/incidents": ".incident-updates-container"
-  };
+  const pathRoot = '/' + window.location.pathname.split('/')[1]
+  const mainContainerMap = {
+    '/': '.masthead-container + .container',
+    '/history': '.months-container',
+    '/incidents': '.incident-updates-container'
+  }
 
   // Add skiplink
-  if (mainContainerMap.hasOwnProperty(pathRoot)) {
-    document.querySelector(mainContainerMap[pathRoot]).setAttribute('id', 'main-content');
-    document.body.insertAdjacentHTML('afterbegin', '<a href="#main-content" class="govuk-skip-link">Skip to main content</a>');
+  if (pathRoot in mainContainerMap) {
+    document.querySelector(mainContainerMap[pathRoot]).setAttribute('id', 'main-content')
+    document.body.insertAdjacentHTML('afterbegin', '<a href="#main-content" class="govuk-skip-link">Skip to main content</a>')
   }
 
-  $('.components-container').removeClass('two-columns').addClass('one-column');
+  $('.components-container').removeClass('two-columns').addClass('one-column')
   // adds autocomplete attributes to controls that don't have one
-  var selectorAndAutocompleteAttributes = {
-    email: "email",
-    phone_country: "tel-country-code",
-    phone_number: "tel-national"
+  const selectorAndAutocompleteAttributes = {
+    email: 'email',
+    phone_country: 'tel-country-code',
+    phone_number: 'tel-national'
   }
 
-  $.each(selectorAndAutocompleteAttributes, function(key, val) {
-    var $targetElements = $(`[name='${key}']`);
-    $.each($targetElements, function() {
-      $(this).attr('autocomplete', val);
-    });
-  });
+  $.each(selectorAndAutocompleteAttributes, function (key, val) {
+    const $targetElements = $(`[name='${key}']`)
+    $.each($targetElements, function () {
+      $(this).attr('autocomplete', val)
+    })
+  })
 
   // a11y fixes for subscribe to ongoing incident(s) button
-  $('.subscribe-button').attr('aria-haspopup', 'dialog').text(function() {
-    return this.text + ' for this incident';
-  });
+  $('.subscribe-button').attr('aria-haspopup', 'dialog').text(function () {
+    return this.text + ' for this incident'
+  })
 
   function removeExpandIncidents () {
     // expand / show all incidents in a given month
     $('.expand-incidents').click().remove()
   }
 
-  removeExpandIncidents();
+  removeExpandIncidents()
 
-  var $logo = document.querySelector('.page-name');
-  var $container = document.querySelector('.layout-content > .container');
-  var $pageFooter = document.querySelector('.page-footer');
+  const $logo = document.querySelector('.page-name')
+  const $container = document.querySelector('.layout-content > .container')
+  const $pageFooter = document.querySelector('.page-footer')
 
   function swapElForHTML ($oldEl, newHTML) {
-    $oldEl.insertAdjacentHTML('beforebegin', newHTML);
-    $oldEl.remove();
+    $oldEl.insertAdjacentHTML('beforebegin', newHTML)
+    $oldEl.remove()
   }
 
   // Rewrite logo
   if ($logo !== null) {
-     $logo.querySelector('a').textContent = 'GOV.UK Notify';
+    $logo.querySelector('a').textContent = 'GOV.UK Notify'
   }
 
   // Add heading to footer
   if ($pageFooter !== null) {
-    $pageFooter.insertAdjacentHTML('afterbegin', '<h2 class="page-footer__heading govuk-visually-hidden">Support links</h2>');
+    $pageFooter.insertAdjacentHTML('afterbegin', '<h2 class="page-footer__heading govuk-visually-hidden">Support links</h2>')
   }
 
   // Home page specific
   if ($container !== null) {
-    if (pathRoot === "/") {
-      var $aboutText = document.querySelector('.page-status + .text-section');
-      var $pageStatus = document.querySelector('.page-status');
-      var $pageStatusContent = $pageStatus.querySelector('h2');
+    if (pathRoot === '/') {
+      const $aboutText = document.querySelector('.page-status + .text-section')
+      const $pageStatus = document.querySelector('.page-status')
+      const $pageStatusContent = $pageStatus.querySelector('h2')
 
       if (($aboutText !== null) && ($pageStatus !== null) && ($pageStatusContent !== null)) {
         // Add headings and move 'about' text
-        $container.insertAdjacentElement('afterbegin', $aboutText);
-        $container.insertAdjacentHTML('afterbegin', '<h1 class="font-x-largest">GOV.UK Notify status page</h1>');
-        $pageStatus.insertAdjacentHTML('beforebegin', '<h2 class="page-status__heading font-largest">Current status</h2>');
+        $container.insertAdjacentElement('afterbegin', $aboutText)
+        $container.insertAdjacentHTML('afterbegin', '<h1 class="font-x-largest">GOV.UK Notify status page</h1>')
+        $pageStatus.insertAdjacentHTML('beforebegin', '<h2 class="page-status__heading font-largest">Current status</h2>')
 
         // Remake status overview from h2 to p
-        swapElForHTML($pageStatusContent, `<p class="${$pageStatusContent.className}">${$pageStatusContent.textContent}</p>`);
+        swapElForHTML($pageStatusContent, `<p class="${$pageStatusContent.className}">${$pageStatusContent.textContent}</p>`)
 
         // Rewrite heading for incidents list to make it clear it includes today
-        document.querySelector('.incidents-list > h2:first-child').textContent = "Recent incidents";
+        document.querySelector('.incidents-list > h2:first-child').textContent = 'Recent incidents'
 
         // Reformat dates
         document.querySelectorAll('.status-day > .date').forEach($el => {
           if (/^[A-Za-z]+\s+\d{1,2},\s+\d{4}$/.test($el.textContent)) {
-            $textNodes = Array.from($el.childNodes).filter($el => $el.nodeType === 3);
-            $el.insertAdjacentElement('afterbegin', $el.querySelector('var:first-of-type'));
-            $textNodes[0].nodeValue = " " + $textNodes[0].nodeValue;
-            $textNodes[1].remove();
+            const $textNodes = Array.from($el.childNodes).filter($el => $el.nodeType === 3)
+            $el.insertAdjacentElement('afterbegin', $el.querySelector('var:first-of-type'))
+            $textNodes[0].nodeValue = ' ' + $textNodes[0].nodeValue
+            $textNodes[1].remove()
           }
-        });
+        })
       }
     }
 
-    if (pathRoot === "/history") {
-      var $pageHeading = $container.querySelector('h4:first-child');
-      var $incidentsList = $container.querySelector('.months-container');
+    if (pathRoot === '/history') {
+      const $pageHeading = $container.querySelector('h4:first-child')
+      const $incidentsList = $container.querySelector('.months-container')
 
       if ($pageHeading !== null) {
-        var pageHeadingText = $pageHeading.textContent;
+        const pageHeadingText = $pageHeading.textContent
 
         // stop camel casing
-        $pageHeading.textContent = pageHeadingText[0].toUpperCase() + pageHeadingText.toLowerCase().slice(1);
-        swapElForHTML($pageHeading, `<h1 class="font-x-largest">${pageHeadingText}</h1>`);
+        $pageHeading.textContent = pageHeadingText[0].toUpperCase() + pageHeadingText.toLowerCase().slice(1)
+        swapElForHTML($pageHeading, `<h1 class="font-x-largest">${pageHeadingText}</h1>`)
       }
 
       function updateIncidentsList () {
-        var $monthHeadings = $incidentsList.querySelectorAll('h4.month-title');
+        const $monthHeadings = $incidentsList.querySelectorAll('h4.month-title')
 
         if ($monthHeadings.length > 0) {
-          $monthHeadings.forEach($el => swapElForHTML($el, `<h2 class="${$el.className}">${$el.textContent}</h2>`));
+          $monthHeadings.forEach($el => swapElForHTML($el, `<h2 class="${$el.className}">${$el.textContent}</h2>`))
         }
       }
 
-      updateIncidentsList();
+      updateIncidentsList()
 
-      var observer = new MutationObserver(() => {
+      const observer = new window.MutationObserver(() => {
         updateIncidentsList()
         removeExpandIncidents()
-      });
-      observer.observe($incidentsList, { attributes: true, childList: true, subtree: true });
+      })
+      observer.observe($incidentsList, { attributes: true, childList: true, subtree: true })
     }
 
-    if (pathRoot === "/incidents") {
-      var $affectedHeading = $container.querySelector('.components-affected');
+    if (pathRoot === '/incidents') {
+      const $affectedHeading = $container.querySelector('.components-affected')
 
       if ($affectedHeading !== null) {
         $affectedHeading.insertAdjacentHTML(
           'afterbegin', '<h2 class="govuk-visually-hidden">Components affected</h2>'
-        );
+        )
       }
     }
   }
-});
+})
