abd7471cc61e1d6edc0b3c48b080634ad6a1e27b
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2025-09-04 14:28:20 +0100

    Add test for history page changes
    Test that incident has a incident descriptor text that matches the incident level (dereived from classname) and that link and incident descriptor are correctly linked via aria-describedby

--- tests/history-page.spec.mjs
+++ tests/history-page.spec.mjs
@@ -44,4 +44,25 @@ describe('History page', () => {
       }
     });
   });
+
+  describe("incident list link", () => {
+    it('should have incident type descriptor below the link and be linked to it vis aria-describedby ', async () => {
+      const incidentLevelMap = {
+        'impact-none': 'No incident',
+        'impact-maintenance': 'Maintenance',
+        'impact-minor': 'Minor incident',
+        'impact-major': 'Major incident',
+        'impact-critical': 'Critical incident'
+      }
+      for await (const el of $$('.incident-data > a')) {
+        const impactLevelClass = (await el.getAttribute('class')).split(" ")[0]
+        const incidentDescriptorElemement = await el.nextElement()
+        const elementAria = await el.getAttribute('aria-describedby')
+        const descriptorElementAria = await incidentDescriptorElemement.getAttribute('id')
+
+        await expect(await incidentDescriptorElemement.getText()).toBe(incidentLevelMap[impactLevelClass])
+        await expect(await elementAria).toBe(descriptorElementAria)
+      }
+    });
+  });
 });
